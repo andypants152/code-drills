4c813da667dddcab7ce8cbabf0c937295d185dbc
Andy M <andy@andypants.xyz>
2018-09-07 10:12:21 -0700

    completed week six, day four

--- week-06/day-04/01/unsolved-harder/index.js
+++ week-06/day-04/01/unsolved-harder/index.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 // We're going to manage our nominees in style with this awesome package
- //(https://www.npmjs.com/package/cli-table.)
+//(https://www.npmjs.com/package/cli-table.)
 var Table = require('cli-table');
 
 
@@ -33,198 +33,208 @@ function review() {
     //Display all of the nominees and their respective data to the command line using CLI Table
     //Once you've done so, invoke the restart function at the end of this function
     connection.query(
-        "SELECT * FROM songs",
+        "SELECT * FROM nominees",
         function (err, res) {
-            if(err) throw err;
+            if (err) throw err;
+
+            var table = new Table({
+                head: ['Show Name', 'Num of Seasons', 'Genre', 'Rating']
+                });
             
+                res.forEach(elem => {
+                    table.push([elem.show_name, elem.num_seasons, elem.genre, elem.rating]);
+                });
+            table.push();
 
-        });
+            console.log("\n" + table.toString());
+        }
+    );
+    restart();
 }
-
 
 function manageNominees() {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "userOptions",
-            message: "What would you like to do",
-            choices: ["Review the nominees", "Add Nominees", "Change a nominee's rating", "Delete a nominee :("]
+            inquirer.prompt([
+                {
+                    type: "list",
+                    name: "userOptions",
+                    message: "What would you like to do",
+                    choices: ["Review the nominees", "Add Nominees", "Change a nominee's rating", "Delete a nominee :("]
+                }
+            ]).then(function (answer) {
+                switch (answer.userOptions) {
+                    //CREATE
+                    case "Add Nominees":
+                        add();
+                        break;
+
+                    //READ
+                    case "Review the nominees":
+                        review();
+                        break;
+
+                    //UPDATE    
+                    case "Change a nominee's rating":
+                        populateNominees(updateRating);
+
+                        // 4. Call the populate nominees function here, and pass it upddateRating as an argument. This will give us an array of options to give to the user when they want to choose who to update;
+                        break;
+
+                    //DELETE    
+                    case "Delete a nominee :(":
+                        //Oooooh, look! ANOTHER callback
+                        populateNominees(remove);
+
+                        // 5. Call the populate nominees function here, and pass it remove as an argument. This will give us an array of options to give to the user when they want to choose who to update;
+                        break;
+
+                }
+            })
         }
-    ]).then(function (answer) {
-        switch (answer.userOptions) {
-            //CREATE
-            case "Add Nominees":
-                add();
-                break;
-
-            //READ
-            case "Review the nominees":
-                review();
-                break;
-
-            //UPDATE    
-            case "Change a nominee's rating":
-                populateNominees(updateRating);
-
-           // 4. Call the populate nominees function here, and pass it upddateRating as an argument. This will give us an array of options to give to the user when they want to choose who to update;
-                break;
-
-            //DELETE    
-            case "Delete a nominee :(":
-            //Oooooh, look! ANOTHER callback
-                populateNominees(remove);
-
-            // 5. Call the populate nominees function here, and pass it remove as an argument. This will give us an array of options to give to the user when they want to choose who to update;
-                break;
-
-        }
-    })
-}
 
 
 //6. 
 //Let's make it so that we can add a nominee to our prestigious list. 
 //As you can see from the prompt, we'll be filling out every column for our nominees' data.
 function add() {
-    inquirer.prompt([{
-        name: "nomiName",
-        message: "\nEnter the name of the show\n"
-    }, {
-        name: "seasons",
-        message: "How many seasons has the show run\n"
-    }, {
-        name: "genre",
-        message: "Enter the genre.\n"
-    }, {
-        name: "rating",
-        message: "Give it a rating out of 10.\n"
+            inquirer.prompt([{
+                name: "nomiName",
+                message: "\nEnter the name of the show\n"
+            }, {
+                name: "seasons",
+                message: "How many seasons has the show run\n"
+            }, {
+                name: "genre",
+                message: "Enter the genre.\n"
+            }, {
+                name: "rating",
+                message: "Give it a rating out of 10.\n"
 
-    }]).then(function(answers){
-
-
-        //7. complete the query string:
-        var queryString = //"????"
-        connection.query(queryString, {
+            }]).then(function (answers) {
 
 
-         //8.
-          //??????
-        })
-        //Let's have a gander at our updated table (pulling straight from our database!!!!)
-        review()
-    })
-}
+                //7. complete the query string:
+                var queryString = "INSERT INTO nominees SET ?"
+                    connection.query(queryString, {
+                        show_name: answers.nomiName,
+                        num_seasons: answers.seasons,
+                        genre: answers.genre,
+                        rating: answers.rating
+                    })
+                //Let's have a gander at our updated table (pulling straight from our database!!!!)
+                review()
+            })
+        }
 //In our inquirer, we could have the user manually type in the name of a show... but what if they mispell?! 
 
 
 //9.
 //Using the values in our table, populate an inquirer list of nominees. 
-function populateNominees(crud){
-    //We don't wan't this array doubling, tripling, etc.. every time we call this function. 
-    options = []
+function populateNominees(crud) {
+            //We don't wan't this array doubling, tripling, etc.. every time we call this function. 
+            options = []
 
 
-    //10. Complete the query string.
-    var queryString = //"?????"
-    connection.query(queryString, function (err, res) {
+            //10. Complete the query string.
+            var queryString = "SELECT show_name FROM nominees";
+                connection.query(queryString, function (err, res) {
+                    //11. LOOP through the names of the nominees, and PUSH them into our options array
+                    res.forEach(function(elem){
+                        options.push(elem.show_name);
+                    })
+                    //Here is our callback. It will call either the update or delete functions depending on what we pass to it in our switch/case
+                    crud(options)
+                })
 
-       
-        //11. LOOP through the names of the nominees, and PUSH them into our options array
-       
-        //Here is our callback. It will call either the update or delete functions depending on what we pass to it in our switch/case
-        crud(options)
-    })
 
-
-}
+        }
 
 //This function will allow the user to update the rating of a show.
 
 function updateRating(list) {
 
-    inquirer.prompt([{
-       name: "nominee",
-       type: "list",
-       message: "Which show do you want to change the rating for",
-       //Remember, list is the options array we passed to this function in our populateNominees function. It is an array containing the most up-to-date list of nominee names
-       choices: list
-    }, {
-        name: "rating",
-        message: "Enter the new rating out of 10 (decimals up to two places are accepted)"
-    }]).then(function(answers){
+            inquirer.prompt([{
+                name: "nominee",
+                type: "list",
+                message: "Which show do you want to change the rating for",
+                //Remember, list is the options array we passed to this function in our populateNominees function. It is an array containing the most up-to-date list of nominee names
+                choices: list
+            }, {
+                name: "rating",
+                message: "Enter the new rating out of 10 (decimals up to two places are accepted)"
+            }]).then(function (answers) {
 
 
-        //12. Complete the query string
-        var queryString = //"????"
-        connection.query(queryString,[
+                //12. Complete the query string
+                var queryString = "UPDATE nominees SET ? WHERE ?"
+                    connection.query(queryString, [{rating: answers.rating},{show_name: answers.nominee}],
 
 
-            //13. Update the rating of the chosen show
-          //?????????
-        ], 
-        function(err, res) {
-            console.log("Here is an updated list of the nominees:")
-            //Let's have a look at our updated table (pulling straight from our database!!!!)
-            review()   
+                        //13. Update the rating of the chosen show
+                        //?????????
+                    
+                        function (err, res) {
+                            console.log("Here is an updated list of the nominees:")
+                            //Let's have a look at our updated table (pulling straight from our database!!!!)
+                            review()
+                        }
+                    )
+            })
+
+
         }
-    )
-    })
-
-    
-}
 
 //This function will remove one show from out database
 function remove(list) {
-    inquirer.prompt([{
-        name: "delete",
-        type: "list",
-        //Remember, list is the options array we passed to this function in our populateNominees function. It is an array containing the most up-to-date list of nominee names
-        choices: options,
-        message: "Who is getting booted from the nominees?"
-    }]).then(function(answer){
+            inquirer.prompt([{
+                name: "delete",
+                type: "list",
+                //Remember, list is the options array we passed to this function in our populateNominees function. It is an array containing the most up-to-date list of nominee names
+                choices: list,
+                message: "Who is getting booted from the nominees?"
+            }]).then(function (answer) {
 
 
-        //14. Complete the query string
-        var queryString = //"?????"
-        connection.query(queryString,{
+                //14. Complete the query string
+                var queryString = "DELETE FROM nominees WHERE ?"
+                    connection.query(queryString, {show_name: answer.delete
 
-            
-            //15. Delete the chosen nominee
-            //This is the row WHERE we will execute our delete
-              //?????
-        })
-        //Let's have a gander at our updated table (pulling straight from our database!!!!)
-        review()
-       
-    })
 
-}
+                        //15. Delete the chosen nominee
+                        //This is the row WHERE we will execute our delete
+                        //?????
+                    })
+                //Let's have a gander at our updated table (pulling straight from our database!!!!)
+                review();
 
-function restart() {
-    inquirer.prompt([{
-        type: "list",
-        name: "continue",
-        choices: ["Yes", "No"],
-        message: "Would you like to do more?\n"
-    }]).then(function(answers) {
-        //If we want to manage more nominees, we'll rerun our initial prompt
-        if (answers.continue === "Yes") {
-            manageNominees();
-            //Otherwise, we'll terminate the process
-        } else {
-           exit()
+            })
+
         }
 
-    });
-}
+function restart() {
+            inquirer.prompt([{
+                type: "list",
+                name: "continue",
+                choices: ["Yes", "No"],
+                message: "Would you like to do more?\n"
+            }]).then(function (answers) {
+                //If we want to manage more nominees, we'll rerun our initial prompt
+                if (answers.continue === "Yes") {
+                    manageNominees();
+                    //Otherwise, we'll terminate the process
+                } else {
+                    exit()
+                }
+
+            });
+        }
 
 
 function exit() {
-    console.log("Thanks for making the all time Emmy's special! Goodbye.")
-    //This will exit out of our command line process
-    connection.end()
-}
-
+            console.log("Thanks for making the all time Emmy's special! Goodbye.")
+            //This will exit out of our command line process
+            connection.end()
+        }
+    
 //This is where the magic begins
 
 //If you've made it here, you should be done!!!!
